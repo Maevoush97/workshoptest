aa81dc117ec5fd6ca63d0a3d6db56b80ef018fb5
JuChoDev <julienchoro@icloud.com>
2024-11-04 11:34:09 +0100

    Salut Maeva !!!

---
+++ workshoptest/HelloView.swift
@@ -0,0 +1,18 @@
+//
+//  HelloView.swift
+//  workshoptest
+//
+//  Created by Julien Choromanski on 04/11/24.
+//
+
+import SwiftUI
+
+struct HelloView: View {
+    var body: some View {
+        Text("Hello, Maeva!")
+    }
+}
+
+#Preview {
+    HelloView()
+}
