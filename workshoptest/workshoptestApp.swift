d28a16d6b82ad10739bd431662e9aee7b0b212e7
Maeva Landre <maevalandrec@gmail.com>
2024-11-04 10:36:41 +0100

    Initial Commit

---
+++ workshoptest/workshoptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshoptestApp.swift
+//  workshoptest
+//
+//  Created by maevalandre on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct workshoptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
