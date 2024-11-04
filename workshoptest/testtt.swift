88ae66d8ced3d8ef7b69137f5f88da8102aba2b3
Maeva Landre <maevalandrec@gmail.com>
2024-11-04 11:32:27 +0100

    TWVUEWJBEW

---
+++ workshoptest/testtt.swift
@@ -0,0 +1,18 @@
+//
+//  testtt.swift
+//  workshoptest
+//
+//  Created by maevalandre on 04/11/24.
+//
+
+import SwiftUI
+
+struct testtt: View {
+    var body: some View {
+        Text("new new nekldenpvenikvenoVOèM")
+    }
+}
+
+#Preview {
+    testtt()
+}
